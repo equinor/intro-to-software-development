93a2b559c97de378603cb6b5e443d1b47f0cd2af
Thomas Sundvoll <thomas@sundvoll.net>
2024-06-07 12:14:35 +0200

    Add gardners equation

--- course_documentation/README.md
+++ course_documentation/README.md
@@ -1,9 +1,11 @@
 # Course Agenda
 
-(Actionable excercises)
+1. Warm up quiz
 
-1. First exercise: Setup package strucutre
+2. First exercise: Setup package strucutre
 
 - Choose a name for your package: "geo-calculator"
 - Create a /src file
 - Create a project file /src/package name
+
+3. Implement Gardner's equation: https://en.wikipedia.org/wiki/Gardner%27s_relation
--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -1,10 +1,31 @@
 # Perform some calculations on the dataset
-import numpy as np
+from typing import Iterable
 
 
-def find_average(numbers: list[float]) -> float:
+def find_average(numbers: Iterable[float]) -> float:
     return sum(numbers) / len(numbers)
 
 
-def numpy_find_average(numbers: list[float]) -> float:
-    return np.average(numbers)
+# Insert typical non-linear domain calculations
+def gardners_equation(velocity: float, alpha=310.0, beta=0.25) -> float:
+    """gardners equation
+
+    https://en.wikipedia.org/wiki/Gardner%27s_relation
+
+    TODO: Include doctest
+    """
+
+    if velocity < 0:
+        raise ValueError(f"Velocity can not be negative, got {velocity}")
+
+    density = alpha * velocity**beta
+    return
+
+
+# Make the inverse garners equation (compute velocity from density)
+def inverse_gardners_equation(density: float):
+    pass
+
+
+# Shear velocity, Vs to density; if time
+# https://en.wikipedia.org/wiki/Shear_modulus
