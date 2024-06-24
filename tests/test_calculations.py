04924568df9a7a4b574ea48646658d3640c44a3f
Thomas Sundvoll <thomas@sundvoll.net>
2024-06-24 14:18:52 +0200

    Implement tests for inverse gardners equation

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -19,17 +19,34 @@ def test_find_average_fails_on_empty_list():
 
 
 def test_gardners_equation():
-    velocity = 2000  # m/s (up to 3500) (water is 1486, lowest possible, given no gas)
-    density = 2000  # kg/m2 [2000-2600]
+    velocity = 2000  # m/s
+    expected_density = 2073.0949  # kg/m2
 
-    assert gardners_equation(velocity) == density
-    assert inverse_gardners_equation(density) == velocity
+    assert gardners_equation(velocity) == pytest.approx(expected_density)
 
-    assert inverse_gardners_equation(gardners_equation(velocity)) == velocity
-    assert gardners_equation(inverse_gardners_equation(density)) == density
+
+def test_inverse_gardners_equation():
+    density = 2073.0949  # kg/m2
+    expected_velocity = 2000  # m/s
+
+    assert inverse_gardners_equation(density) == pytest.approx(expected_velocity)
+
+    assert inverse_gardners_equation(
+        gardners_equation(expected_velocity)
+    ) == pytest.approx(expected_velocity)
+
+    assert gardners_equation(inverse_gardners_equation(density)) == pytest.approx(
+        density
+    )
 
 
 def test_gardners_equation_negative_velocity():
-    velocity = -10  # m/s
+    velocity = -1000  # m/s
     with pytest.raises(ValueError) as e:
         gardners_equation(velocity)
+
+
+def test_inverse_gardners_equation_negative_velocity():
+    density = -1000  # kg/m3
+    with pytest.raises(ValueError) as e:
+        inverse_gardners_equation(density)
