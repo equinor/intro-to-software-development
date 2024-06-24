49dab7f841e93cf0000a5146575fdaa2bcc0f474
Thomas Sundvoll <thomas@sundvoll.net>
2024-06-24 14:06:42 +0200

    Implement inverse gardners equation and add docstrings

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -1,19 +1,26 @@
-# Perform some calculations on the dataset
-from typing import Iterable
+# Perform some calculations for use on lithology datasets
 
 
-def find_average(numbers: Iterable[float]) -> float:
+def find_average(numbers: list[float]) -> float:
     return sum(numbers) / len(numbers)
 
 
-# Insert typical non-linear domain calculations
 def gardners_equation(
     velocity: float, alpha: float = 310.0, beta: float = 0.25
 ) -> float:
-    """gardners equation
+    """Calculate bulk density of lithology based on seismic P-wave velocity
 
+    Args:
+        velocity (float): Seismic P-wave velocity in m/s.
+        alpha (float): Empirically derived constant.
+        beta (float): Empirically derived constant.
+    Returns:
+        density (float): Bulk density in kg/m3.
+    Raises:
+        ValueError: If the velocity is negative.
+
+    Read more about gardners equation here:
     https://en.wikipedia.org/wiki/Gardner%27s_relation
-
     """
 
     if velocity < 0:
@@ -23,6 +30,24 @@ def gardners_equation(
     return density
 
 
-# Make the inverse garners equation (compute velocity from density)
-def inverse_gardners_equation(density: float):
-    pass
+def inverse_gardners_equation(
+    density: float, alpha: float = 310.0, beta: float = 0.25
+) -> float:
+    """Calculate seismic P-wave velocity based on bulk density of lithology
+
+    Args:
+        density (float): Bulk density in kg/m3.
+        alpha (float): Empirically derived constant.
+        beta (float): Empirically derived constant.
+    Returns:
+        velocity (float): Seismic P-wave velocity in m/s.
+    Raises:
+        ValueError: If the density is negative.
+
+    """
+
+    if density < 0:
+        raise ValueError(f"Density can not be negative, got {density}")
+
+    velocity: float = (density / alpha) ** (1 / beta)
+    return velocity
