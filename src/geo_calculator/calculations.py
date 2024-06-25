2ccacb40cc06c846072279e8bd84293f0cb823f0
Jonathan Eliah Amdam <jonathan@amdam.com>
2024-06-25 11:03:05 +0000

    Implemented Gardners equation

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -1,2 +1,5 @@
 def find_average(numbers):
     return sum(numbers)/len(numbers)
+
+def gardners_equation(velocity):
+    return 0.31*(velocity**0.25)
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,5 +1,14 @@
 from geo_calculator.calculations import find_average
+from geo_calculator.calculations import gardners_equation
+import pytest
 
 def test_find_average_given_a_list_of_ints() -> None:
     numbers = [1, 2, 3, 4, 5, 6]
     assert find_average(numbers) == 3.5
+
+def test_gardners_equation():
+    velocity = 2000  # m/s
+    expected_density = 2.0730949  # g/cm3
+
+    # By default, approx considers numbers within a relative tolerance of 1e-6
+    assert gardners_equation(velocity) == pytest.approx(expected_density)
