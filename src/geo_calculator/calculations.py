96634f83745095a88bf97ad5e886fd257bd8fd10
Sebastian Ellingsen <97960964+sebasell@users.noreply.github.com>
2024-06-25 13:03:40 +0000

    Add code formatting check

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -1,7 +1,7 @@
 def find_average(num_list):
     if len(num_list) == 0:
-        return 0  
+        return 0
     average = 0
-    for i in num_list:  
+    for i in num_list:
         average += i
-    return average / len(num_list)  
+    return average / len(num_list)
