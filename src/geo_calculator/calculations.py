d76debbd0cb36251d944633d5a508b726fb6d0eb
Jonathan Eliah Amdam <jonathan@amdam.com>
2024-06-25 12:18:21 +0000

    Added documentation

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -1,5 +1,15 @@
 def find_average(numbers: list) -> float:
+    """Calculate the average of some numbers
+    Args:
+        numbers: A sequence of numbers
+    Returns:
+        The average value of the numbers
+    """
     return sum(numbers) / len(numbers)
 
 def gardners_equation(velocity: float) -> float:
+    """Calculates Gardner's equation
+    Args:
+        velocity: A float in m/s
+    """
     return 0.31*(velocity**0.25)
