d020d9b3bc9d88d5eeaa06ef2d40b288dfe34faf
Starcko <93937327+Starcko@users.noreply.github.com>
2024-06-26 09:29:21 +0000

    gardners equation

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -1,2 +1,12 @@
 def find_average(nums: list[int]) -> float:
     return sum(nums) / len(nums)
+
+def gardners_equation(velocity: int, alpha: float = 0.31, beta: float = 0.25) -> float:
+    """Calculates the seismic P-wave velocity to the bulk density of the lithology in which the wave travels.
+    Args:
+        velocity: A number
+    Returns:
+        The bulk density
+    """
+    density = alpha * velocity ** beta
+    return density
