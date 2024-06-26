09915a8e7e817e363d20c858fb557a78b36c1a9e
Emma Andrine Bergheim Brændhagen <123013110+emmaandrine@users.noreply.github.com>
2024-06-26 14:09:21 +0000

    Implement inverse gardner's equation

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -1,4 +1,23 @@
 from typing import Sequence
 
-def find_average(numbers: Sequence[int]) ->float:
+
+def find_average(numbers: Sequence[int]) -> float:
+    """Calculate the average of some numbers
+    Args:
+        numbers: A sequence of numbers
+    Returns:
+        The average value of the numbers
+    """
     return sum(numbers) / len(numbers)
+
+
+def gardners_equation(velocity: float) -> float:
+    """Calculate bulk densisty of the lithology
+    Args:
+        Velocity: a float 
+    Returns: 
+        The density
+    """
+    alfa: float = 0.31
+    beta: float  = 0.25
+    return alfa * velocity**beta
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,6 +1,15 @@
-from geo_calculator.calculations import find_average
+import pytest
+from geo_calculator.calculations import find_average, gardners_equation
 
 
 def test_find_average_given_a_list_of_ints() -> None:
     numbers = [1, 2, 3, 4, 5, 6]
     assert find_average(numbers) == 3.5
+
+
+def test_gardners_equation():
+    velocity = 2000  # m/s
+    expected_density = 2.0730949  # g/cm3
+
+    # By default, approx considers numbers within a relative tolerance of 1e-6
+    assert gardners_equation(velocity) == pytest.approx(expected_density)
