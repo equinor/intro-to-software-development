106f067f77e20affcf7ae0f70c5c00900cd3ecef
Olav Irgens Henanger <oihe@equinor.com>
2024-06-26 08:36:23 +0000

    format

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -1,11 +1,14 @@
 from typing import Sequence
 
+
 def find_average(numbers: Sequence[int]) -> float:
     return sum(numbers) / len(numbers)
 
+
 def use() -> None:
-    my_average: float = find_average([1,2,3])
+    my_average: float = find_average([1, 2, 3])
     print(my_average)
+
 
 if __name__ == "__main__":
     use()
