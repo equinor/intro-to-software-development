3f6aa8224381b6474cf209f6a564864fb3f62609
Sunniva Rostad <122443127+Sunnivar@users.noreply.github.com>
2024-06-26 08:07:16 +0000

    test

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -1,8 +1,12 @@
-def find_average(numbers):
+from typing import Sequence
+
+def find_average(numbers: Sequence[int]) -> float:
     tot = 0
     for num in numbers:
         tot += num
 
-
-
     return tot/len(numbers)
+
+def gardners_equation(velocity: float) -> float:
+    rho = 0.23*velocity**(0.25)
+    return rho
