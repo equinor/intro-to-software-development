efda492cba909e13c9df615c87a086a2c839c4ff
Olav Irgens Henanger <oihe@equinor.com>
2024-06-26 09:38:49 +0000

    testing negatives and inverse gardners

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -20,7 +20,17 @@ def gardners_equation(vel) -> float:
     Returns:
         The bulk density in g/cm^3
     """
+    if vel<0:
+        raise ValueError(f"Negative velocity value is not physical/possible. Got {vel}.")
     return 0.31*vel**(0.25)
+
+def inverse_gardners_equation(dense) -> float:
+    """
+    Inverse gardners equation, finds velocity with density input
+    """
+    if dense<0:
+        raise ValueError(f"Negative density is not physical, and the input was {dense}")
+    return (dense/0.31)**4
 
 if __name__ == "__main__":
     use()
