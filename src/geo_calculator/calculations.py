712fd957b6d55ca03f38c8b8c65d0c2e40c78d1d
BenjaminDanielsen <111968228+BenjaminDanielsen@users.noreply.github.com>
2024-06-26 09:14:58 +0000

    Add docstrings, gardners equation and test for gardners equation

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -1,3 +1,18 @@
 def find_average(list: list) -> float:
-    """Returns the avreage of a list"""
+    """Calculate the average of some numbers
+    Args:
+        numbers: A sequence of numbers
+    Returns:
+        The average value of the numbers
+    """
     return sum(list) / len(list)
+
+
+def gardners_equation(velocity: float) -> float:
+    """Calculate the density
+    Args:
+        velocity: A float in m/s
+    Returns:
+        The density
+    """
+    return 0.31 * velocity ** (1 / 4)
