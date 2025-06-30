5ccde1b5d61b2618340e9ba88e4d5dc5a9a4c21a
Leo Lie McGrath <93575375+leomcgrath@users.noreply.github.com>
2025-06-30 12:59:58 +0000

    Added docstrings to gardners_equation and find_average

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -1,8 +1,22 @@
 def find_average(data_list: list) -> float:
+    """Calculate the average of some numbers
+    Args:
+        numbers: A sequence of numbers
+    Returns:
+        The average value of the numbers
+    """
+
     sum = 0
     for i in data_list:
         sum += i
     return sum/len(data_list)
 
-def gardners_equation(velocity):
+def gardners_equation(velocity) -> float:
+    """Calculate the seismic p-wave velocity from the bulk density of the lithology in which the wave travels.
+    Args:
+        Velocity
+    Returns:
+        Seismic p-wave velocity
+    """
+
     return 0.31*(velocity**0.25)
