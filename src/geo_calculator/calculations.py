876e6dd33676acf8c46997676240bc571c171330
Leo Lie McGrath <93575375+leomcgrath@users.noreply.github.com>
2025-06-30 12:49:29 +0000

    Gardners equation implemented

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -3,3 +3,6 @@ def find_average(data_list: list) -> float:
     for i in data_list:
         sum += i
     return sum/len(data_list)
+
+def gardners_equation(velocity):
+    return 0.31*(velocity**0.25)
