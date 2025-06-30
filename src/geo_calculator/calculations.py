2dd0ced15df0436d1205581bfa452e8d8bc4a20b
Dawid Szczur <79862692+davethemultitasker@users.noreply.github.com>
2025-06-30 12:54:06 +0000

    Add gardners equation

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -2,3 +2,6 @@ from typing import List
 
 def find_average(numbers: List[int | float]) -> float:
     return sum(numbers) / len(numbers)
+
+def gardners_equation(velocity: float) -> float:
+    return 0.31 * velocity^0.25
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -7,3 +7,10 @@ def test_lenght_of_string() -> None:
 def test_find_avrage_of_list_of_numbers() -> None:
     test_list = [1,2,3,4,5,6]
     assert find_average(test_list) == 3.5
+
+def test_gardners_equation():
+    velocity = 2000  # m/s
+    expected_density = 2.0730949  # g/cm3
+
+    # By default, approx considers numbers within a relative tolerance of 1e-6
+    assert gardners_equation(velocity) == pytest.approx(expected_density)
