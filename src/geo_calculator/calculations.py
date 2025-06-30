b978b3dfa3d7c28c34045d11463d48134a938f60
Victoria Myklebust Olsen <vols@equinor.com>
2025-06-30 11:04:35 +0000

    New test calculations

---
+++ src/geo_calculator/calculations.py
@@ -0,0 +1,4 @@
+def find_average(numbers):
+    if not numbers:
+        return 0.0  
+    return sum(numbers) / len(numbers) 
---
+++ test_calculations.py
@@ -0,0 +1,11 @@
+from geo_calculator.calculations import find_average
+
+def test_length_of_string() -> None:
+    test_string = "python"
+    assert len(test_string) == 6
+
+def test_find_average_of_list_of_numbers() -> None:
+    test_list = [1,2,3,4,5,6]
+    assert find_average(test_list) == 3.5
+
+
