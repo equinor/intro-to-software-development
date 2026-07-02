60bf1c2ef35fa5f1d5e3b8fc363c1ea272f56a03
Valerii Martell <vamar@equinor.com>
2026-07-02 08:40:39 +0000

    Add bad format

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -1,2 +1,3 @@
 def find_average(numbers):
+    a = numbers[0]
     return sum(numbers) / len(numbers)
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,7 +1,8 @@
 from geo_calculator.calculations import find_average
 
+
 def test_find_average_given_list_of_numbers():
-    numbers = [1,2,3,4,5,6]
+    numbers = [1, 2, 3, 4, 5, 6]
 
     result = find_average(numbers)
 
